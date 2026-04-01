98fb337d991fecc015704a27a96bc26fd66fb417
saifali-arswift <saifali@arswift.com>
2026-04-01 23:44:09 +0500

    feat(todo-list): Add CategoryFilter component and implement category-based filtering for todos

--- my-todo-app/src/App.jsx
+++ my-todo-app/src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react';
 import TodoList from './components/TodoList';
 import AddTodo from './components/AddTodo';
-
+import CategoryFilter from './components/CategoryFilter';
 export default function App () {
   const [todos, setTodos] = useState([
     { id: 1, text: 'Understand React', completed: false, category: 'Work' },
-    { id: 2, text: 'freeCodeCamp', completed: false, category: 'Work' },
+    { id: 2, text: 'freeCodeCamp', completed: false, category: 'Personal' },
     { id: 3, text: 'LinkedIn connections', completed: false, category: 'Work' },
   ]);
+
+  const [activeCategory, setActiveCategory] = useState('All');
+
+  const filteredJobs =
+    activeCategory === 'All'
+      ? todos
+      : todos.filter(todo => todo.category === activeCategory);
 
   function handleAddTodo (text) {
     const newTodo = {
@@ -21,11 +28,12 @@ export default function App () {
   function handleDelete (id) {
     setTodos(todos.filter(todo => todo.id !== id));
   }
+
   function handleToggle (id) {
     setTodos(
-      todos.map(todo => {
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo;
-      })
+      todos.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
     );
   }
 
@@ -33,7 +41,15 @@ export default function App () {
     <div>
       <h1>My Todo App</h1>
       <AddTodo onAdd={handleAddTodo} />
-      <TodoList todos={todos} onDelete={handleDelete} onToggle={handleToggle} />
+      <CategoryFilter
+        activeCategory={activeCategory}
+        setActiveCategory={setActiveCategory}
+      />
+      <TodoList
+        todos={filteredJobs}
+        onDelete={handleDelete}
+        onToggle={handleToggle}
+      />
     </div>
   );
 }
--- my-todo-app/src/components/CategoryFilter.jsx
+++ my-todo-app/src/components/CategoryFilter.jsx
@@ -1,10 +1,21 @@
-export default function CategoryFilter ({ activeCategory, setActiveCategory }) {
+function CategoryFilter ({ activeCategory, setActiveCategory }) {
   const categories = ['Work', 'Personal', 'All'];
   return (
     <div className='flex gap-2 mb-4'>
-      {categories.map(cat => {
-        <button key={cat} onClick={setActiveCategory(cat)}></button>;
-      })}
+      {categories.map(cat => (
+        <button
+          key={cat}
+          onClick={() => setActiveCategory(cat)}
+          className={`px-3 py-1 rounded ${
+            activeCategory === cat
+              ? 'bg-blue-500 text-white'
+              : 'bg-gray-200 text-gray-700'
+          }`}
+        >
+          {cat}
+        </button>
+      ))}
     </div>
   );
 }
+export default CategoryFilter;
